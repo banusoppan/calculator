57fbe09cb2e349ca24b3419e1ada949e880f7f60
Ishfaq <ahamed@netmaiesta.co.uk>
2019-05-29 17:41:11 +0530

    adding numbers to calculator

---
+++ src/main/java/com/example/calculator/CalculatorAppliction.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+public class CalculatorAppliction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator=new Calculator();
+		int num=calculator.sum(2, 3);
+		System.out.println(num);
+
+	}
+
+}
